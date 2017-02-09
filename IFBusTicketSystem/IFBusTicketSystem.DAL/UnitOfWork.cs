eda274ab2f581f5d5d2461cbec185441d1f7df7e
vovayevdoshenko <vovayevdoshenko@gmail.com>
2017-02-09 23:37:50 +0200

    1: Add basic content to Unit of Work

--- IFBusTicketSystem/IFBusTicketSystem.DAL/UnitOfWork.cs
+++ IFBusTicketSystem/IFBusTicketSystem.DAL/UnitOfWork.cs
@@ -1,23 +1,58 @@
-﻿using System;
+﻿using FluentNHibernate.Cfg;
 using IFBusTicketSystem.DAL.Interfaces;
+using NHibernate;
 
 namespace IFBusTicketSystem.DAL
 {
     public class UnitOfWork: IUnitOfWork
     {
+        private static readonly ISessionFactory SessionFactory;
+        private ITransaction _transaction;
+
+        public ISession Session { get; } = SessionFactory.OpenSession();
+
+        static UnitOfWork()
+        {
+            //TODO: Add SessionFactory configuration later (V.Y.)
+            SessionFactory = Fluently.Configure().BuildSessionFactory();
+        }
+
         public void BeginTransaction()
         {
-            throw new NotImplementedException();
+            _transaction = Session.BeginTransaction();
         }
 
         public void Commit()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Commit();
+            }
+            catch
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Rollback();
+
+                throw;
+            }
+            finally
+            {
+                Session.Dispose();
+            }
         }
 
         public void Rollback()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (_transaction != null && _transaction.IsActive)
+                    _transaction.Rollback();
+            }
+            finally
+            {
+                Session.Dispose();
+            }
         }
     }
 }
