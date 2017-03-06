caa849ab8b0bf20c562a0adbeb1fa8e5104c493b
vovayevdoshenko <vovayevdoshenko@gmail.com>
2017-03-07 00:53:51 +0200

    fix

--- IFBusTicketSystem/IFBusTicketSystem.DAL/Repositories/UserRepository.cs
+++ IFBusTicketSystem/IFBusTicketSystem.DAL/Repositories/UserRepository.cs
@@ -9,28 +9,28 @@ namespace IFBusTicketSystem.DAL.Repositories
 {
     public class UserRepository : Repository<UserInfo>, IUserRepository, IDisposable
     {
-        private readonly UserManager<UserInfo> _userManager;
+        //private readonly UserManager<UserInfo> _userManager;
 
-        public UserRepository()
-        {
-            _userManager = new UserManager<UserInfo>(new UserStore<UserInfo>(Session));
-        }
+        //public UserRepository()
+        //{
+        //    _userManager = new UserManager<UserInfo>(new UserStore<UserInfo>(Session));
+        //}
 
-        public async Task<IdentityResult> Register(UserInfo user, string password)
-        {
-            return await _userManager.CreateAsync(user, password);
-        }
+        //public async Task<IdentityResult> Register(UserInfo user, string password)
+        //{
+        //    return await _userManager.CreateAsync(user, password);
+        //}
 
-        public async Task<UserInfo> FindUser(string userName, string password)
-        {
-            var user = await _userManager.FindAsync(userName, password);
+        //public async Task<UserInfo> FindUser(string userName, string password)
+        //{
+        //    var user = await _userManager.FindAsync(userName, password);
 
-            return user;
-        }
+        //    return user;
+        //}
 
-        public void Dispose()
-        {
-            _userManager.Dispose();
-        }
+        //public void Dispose()
+        //{
+        //    _userManager.Dispose();
+        //}
     }
 }
